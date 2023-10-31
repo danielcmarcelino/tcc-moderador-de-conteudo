28d0fd6f2af701ece78c64636ef80303ae67f41a
Daniel Marcelino <danielmarcelino.ft@gmail.com>
2023-10-31 13:48:26 -0300

    Alterações do Lucas

--- algoritmoWord2Vec.py
+++ algoritmoWord2Vec.py
@@ -1,47 +1,31 @@
 from geral import *
-import os 
 import numpy as np
 import pandas as pd
+import os
 import re
 from gensim.models import Word2Vec
 from joblib import dump, load
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
-from sklearn.svm import SVC
+from sklearn.linear_model import Perceptron
+from sklearn.neural_network import MLPClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import SGDClassifier, PassiveAggressiveClassifier
+from unidecode import unidecode
 from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, f1_score
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.linear_model import Perceptron, SGDClassifier, PassiveAggressiveClassifier
-from unidecode import unidecode
 
 caminhoArquivos = 'arquivos/'
-
-caminhoWord2Vec = caminhoArquivos + 'word2vec/'  # Novo caminho para salvar o modelo - 
-
-
+caminhoWord2Vec = caminhoArquivos + 'word2vec/'
 nomeArquivo = caminhoArquivos + 'comentarios_toxicos_ptBR.csv'
 nomeColTexto = 'text'
 nomeColTexto2 = 'text_norm'
 nomeColRotulo = 'toxic'
-
-caminhoArquivos = caminhoArquivos + 'word2vec/'
-nomeArquivoModelo = caminhoArquivos + 'modelo.bin'
+nomeArquivoModelo = caminhoWord2Vec + 'modelo_word2vec_skipgram.bin'
+nomeArquivoClassificadorSkipGram = caminhoArquivos + 'classificador_word2vec_skipgram.joblib'
 
 if not os.path.exists(caminhoWord2Vec):
     os.makedirs(caminhoWord2Vec)
-
-
-def converterTextoParaVetores(texto, modelo):
-    try:
-        vetores = []
-        for palavra in texto:
-            if palavra in modelo.wv:
-                vetores.append(modelo.wv[palavra])
-        if vetores:
-            return np.mean(vetores, axis=0)
-        else:
-            return np.zeros(modelo.vector_size)
-    except Exception as e:
-        raise Exception('Arquivo "algoritmoWord2Vec", método "converterTextoParaVetores": \n' + str(e))
 
 def lerArquivo():
     try:
@@ -54,90 +38,62 @@ def lerArquivo():
 def tratarDataframe(df):
     try:
         print('Tamanho inicial do dataframe = ' + str(df.shape))
-
-        #Unificando as colunas de texto e duplicando a rótulo para não perder a consistência de dados
         df_ColTexto1 = df[[nomeColTexto,  nomeColRotulo]]
         df_ColTexto2 = df[[nomeColTexto2, nomeColRotulo]]
         df_ColTexto2 = df_ColTexto2.rename(columns={nomeColTexto2: nomeColTexto})
         df_novo = pd.concat([df_ColTexto1, df_ColTexto2], ignore_index=True, axis=0, keys=None, names=[nomeColTexto, nomeColRotulo])
 
-        #Removendo linhas com valores nulos
         print('Valores nulos = ' + str(df_novo.isnull().sum().sum()))
         df_novo = df_novo.dropna().reset_index(drop=True)
-
-        #Substituindo caracteres acentuados por seus respectivos sem acentuação
         df_novo[nomeColTexto] = df_novo[nomeColTexto].apply(lambda x: unidecode(x))
-
-        #Removendo caracteres que não sejam letras ou números
         df_novo[nomeColTexto] = df_novo[nomeColTexto].apply(lambda x: re.sub(r'[^a-zA-Z0-9\s]', '', x))
-
         print('Tamanho final do dataframe = ' + str(df_novo.shape))
         return df_novo
     except Exception as e:
         raise Exception('Arquivo "algoritmoWord2Vec", método "tratarDataframe": \n' + str(e))
 
-def treinarWord2Vec(algoritmoTreinoWord2Vec=0, tamanhoTeste=0.3, algoritmoClassificador = 'RFC'):
+def treinarModeloWord2Vec(modelo_word2vec, X_treino, rotulos_treino, textos_teste, rotulos_teste, nome_arquivo_classificador):
     try:
-        if algoritmoClassificador not in ['RFC', 'SVM', 'NB', 'ADA', 'PER', 'SGD', 'PA']:
-            raise Exception('algoritmoClassificador inválido')
-        
-        removerArquivo(nomeArquivoModelo)
-        
-        df = lerArquivo()
+        # Lista de classificadores a serem treinados
+        classificadores = [
+            Perceptron(),
+            MLPClassifier(),
+            DecisionTreeClassifier(),
+            AdaBoostClassifier(),
+            RandomForestClassifier(),
+            MultinomialNB(),
+            SGDClassifier(),
+            PassiveAggressiveClassifier()
+        ]
 
-        textos = [t.lower().split() for t in df[nomeColTexto]]
-        rotulos = [a for a in df[nomeColRotulo]]
+        for classificador in classificadores:
+            # Treinando o classificador
+            print(f'\nInício do treinamento {classificador.__class__.__name__} com Word2Vec ({modelo_word2vec})')
+            classificador.fit(X_treino, rotulos_treino)
+            # Salvando o classificador treinado para uso futuro
+            dump(classificador, nome_arquivo_classificador)
+            print(f'Fim do treinamento {classificador.__class__.__name__} com Word2Vec ({modelo_word2vec})\n')
 
-        textos_treino, textos_teste, rotulos_treino, rotulos_teste = train_test_split(textos, rotulos, test_size=tamanhoTeste, random_state=42)
+            # Avaliando a acurácia no conjunto de teste
+            X_teste = [modelo_word2vec.wv[token] for token in textos_teste]
+            predicoes = classificador.predict(X_teste)
+            acuracia = accuracy_score(rotulos_teste, predicoes)
+            print(f'Acurácia para {classificador.__class__.__name__} com Word2Vec ({modelo_word2vec}):', acuracia)
 
-        msgAux = ' do treinamento Word2Vec utilizando algoritmo de treino ' + ('CBOW' if algoritmoTreinoWord2Vec == 0 else 'Skip-Gram')
-        print('\nInício' + msgAux)
-        modelo = Word2Vec(textos_treino, min_count=1, sg=algoritmoTreinoWord2Vec)
-        modelo.save(nomeArquivoModelo)
-        print('Fim' + msgAux + '\n')
+            # Calculando métricas para os resultados do TCC
+            matriz_confusao = confusion_matrix(rotulos_teste, predicoes)
+            tvb = matriz_confusao[0, 0] / (matriz_confusao[0, 0] + matriz_confusao[0, 1])
+            tfd = matriz_confusao[1, 0] / (matriz_confusao[1, 0] + matriz_confusao[1, 1])
+            tpf = matriz_confusao[0, 1] / (matriz_confusao[0, 0] + matriz_confusao[0, 1])
+            f_medida = f1_score(rotulos_teste, predicoes)
 
-        # Convertendo textos em vetores de palavras
-        X_treino = np.array([converterTextoParaVetores(texto, modelo) for texto in textos_treino])
-        X_teste = np.array([converterTextoParaVetores(texto, modelo) for texto in textos_teste])
+            print(f'Taxa de Verdadeiros Negativos (TVB) para {classificador.__class__.__name__} com Word2Vec ({modelo_word2vec}):', tvb)
+            print(f'Taxa de Falsos Negativos (TFD) para {classificador.__class__.__name__} com Word2Vec ({modelo_word2vec}):', tfd)
+            print(f'Taxa de Falsos Positivos (TPF) para {classificador.__class__.__name__} com Word2Vec ({modelo_word2vec}):', tpf)
+            print(f'F-medida para {classificador.__class__.__name__} com Word2Vec ({modelo_word2vec}):', f_medida)
 
-        nomeArquivoClassificador = caminhoArquivos + 'classificador' + algoritmoClassificador + '.joblib'
-        removerArquivo(nomeArquivoClassificador)
-        print('\nInício do treinamento do algoritmo de classificação ' +  algoritmoClassificador)
-        if algoritmoClassificador == 'SVM':
-            classificador = SVC()  
-        elif algoritmoClassificador == 'NB':
-            classificador = MultinomialNB()
-        elif algoritmoClassificador == 'ADA':
-            classificador = AdaBoostClassifier()
-        elif algoritmoClassificador == 'PER':
-            classificador = Perceptron()
-        elif algoritmoClassificador == 'SGD':
-            classificador = SGDClassifier()
-        elif algoritmoClassificador == 'PA':
-            classificador = PassiveAggressiveClassifier()
-        else:
-            classificador = RandomForestClassifier()
-        classificador.fit(X_treino, rotulos_treino)
-        dump(classificador, nomeArquivoClassificador)
-        print('Fim do treinamento do algoritmo de classificação ' +  algoritmoClassificador + '\n')
-        
-        predicoes = classificador.predict(X_teste)
-        acuracia = accuracy_score(rotulos_teste, predicoes)
-        print('Acurácia:', acuracia)
-
-        # Calculando outras métricas
-        matriz_confusao = confusion_matrix(rotulos_teste, predicoes)
-        tvb2 = matriz_confusao[0, 0] / (matriz_confusao[0, 0] + matriz_confusao[0, 1])
-        tfd2 = matriz_confusao[1, 0] / (matriz_confusao[1, 0] + matriz_confusao[1, 1])
-        tpf2 = matriz_confusao[0, 1] / (matriz_confusao[0, 0] + matriz_confusao[0, 1])
-        f_medida2 = f1_score(rotulos_teste, predicoes)
-
-        print('Taxa de Verdadeiros Negativos (TVB):', tvb2)
-        print('Taxa de Falsos Negativos (TFD):', tfd2)
-        print('Taxa de Falsos Positivos (TPF):', tpf2)
-        print('F-medida:', f_medida2)
     except Exception as e:
-        raise Exception('Arquivo "algoritmoWord2Vec", método "treinarWord2Vec": \n' + str(e))
+        raise Exception(f'Erro durante o treinamento dos modelos com Word2Vec: \n{str(e)}')
 
 def validarTextoWord2Vec(texto, algoritmoClassificador):
     try:
@@ -156,3 +112,33 @@ def validarTextoWord2Vec(texto, algoritmoClassificador):
         return predicao <= 0.5
     except Exception as e:
         raise Exception('Arquivo "algoritmoWord2Vec", método "validarTexto": \n' + str(e))
+
+def converterTextoParaVetores(tokens, modelo):
+    # Função auxiliar para converter um texto em uma sequência de vetores usando um modelo Word2Vec
+    vetor_resultante = []
+    for token in tokens:
+        if token in modelo.wv:
+            vetor_resultante.append(modelo.wv[token])
+    return np.mean(vetor_resultante, axis=0)
+
+def main():
+    try:
+        # Lendo o arquivo para obter o dataframe
+        df = lerArquivo()
+
+        # Treinando modelo Word2Vec com Skip-gram
+        modelo_word2vec_skipgram = Word2Vec(sentences=df[nomeColTexto].apply(lambda x: x.split()), vector_size=100, window=5, sg=1, min_count=1)
+        nome_arquivo_classificador_skipgram = caminhoArquivos + 'classificador_word2vec_skipgram.joblib'
+        treinarModeloWord2Vec(modelo_word2vec_skipgram, df[nomeColTexto], df[nomeColRotulo], df[nomeColTexto], df[nomeColRotulo], nome_arquivo_classificador_skipgram)
+
+        # Exemplo de uso da função validarTextoWord2Vec
+        texto_para_validar = "Seu texto aqui."
+        algoritmo_classificador_para_usar = "Perceptron"  # Substitua pelo classificador desejado
+        resultado_validacao = validarTextoWord2Vec(texto_para_validar, algoritmo_classificador_para_usar)
+        print(f"Resultado da validação: {resultado_validacao}")
+
+    except Exception as e:
+        raise Exception(f'Erro na função principal: \n{str(e)}')
+
+if __name__ == '__main__':
+    main()
